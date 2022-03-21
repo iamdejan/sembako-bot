c22b6a42406ee3cd8aa04a1ecc06a1231cf038de
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-03-21 23:37:29 +0700

    Remove service account

--- 02-service-account.tf
+++
@@ -1,4 +0,0 @@
-resource "google_service_account" "sembako_account" {
-  account_id   = "sembako-scheduler"
-  display_name = "Service Account for Cloud Scheduler"
-}
--- 03-cloud-scheduler.tf
+++ 03-cloud-scheduler.tf
@@ -8,9 +8,5 @@ resource "google_cloud_scheduler_job" "sembako" {
   http_target {
     http_method = "POST"
     uri         = "${google_cloud_run_service.sembako.status[0].url}/prices"
-
-    oidc_token {
-      service_account_email = google_service_account.sembako_account.email
-    }
   }
 }
