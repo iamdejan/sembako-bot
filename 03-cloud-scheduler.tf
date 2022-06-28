fdc80fa56b29ed452c6ec71646fe31607ae2112c
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-06-29 00:18:00 +0700

    Add proper retry config

--- 03-cloud-scheduler.tf
+++ 03-cloud-scheduler.tf
@@ -5,6 +5,13 @@ resource "google_cloud_scheduler_job" "sembako" {
   time_zone        = var.time_zone
   attempt_deadline = "600s"
 
+  retry_config {
+    min_backoff_duration = "1s"
+    max_retry_duration   = "60s"
+    max_doublings        = 2
+    retry_count          = 3
+  }
+
   http_target {
     http_method = "POST"
     uri         = "${google_cloud_run_service.sembako.status[0].url}/prices"
